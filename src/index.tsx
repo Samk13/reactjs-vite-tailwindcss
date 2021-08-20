d62e9399b3aec517562bdf2662c7ce8bb55335c6
Samk13 <samarbid13@gmail.com>
2021-08-21 00:48:45 +0200

    add comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Sam Arbid Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'tailwindcss/tailwind.css'
